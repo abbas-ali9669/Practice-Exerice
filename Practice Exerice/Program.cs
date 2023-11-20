cb58f540a2cce5183056c7b370cb260bfc8699bc
abbas-ali9669 <abbas.ali9669@hotmail.com>
2023-11-20 18:41:46 +0500

    Chnage the name of the vowel function.

--- Practice Exerice/Program.cs
+++ Practice Exerice/Program.cs
@@ -580,7 +580,7 @@ namespace Practice_Exerice
 
         // Exercise 25
         // Check the vowel 
-        public static void checkVowel()
+        public static void checkVowellllll()
         {
             Console.Write("Enter a vowel alphabet: ");
             char alph = char.Parse(Console.ReadLine());
